7169cd722678140b6481d416eeaa7a28e352bc00
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-09-13 17:38:09 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,23 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    Wiederholung = false
+    Ampelsteuerung()
+})
 function Ampelsteuerung () {
     basic.pause(500)
     AAmpel(0, 0, 1)
     FAmpel(1, 0)
+    basic.pause(1000)
+    AAmpel(0, 1, 0)
+    basic.pause(2000)
+    AAmpel(1, 0, 0)
+    basic.pause(2000)
+    FAmpel(0, 1)
+    basic.pause(7000)
+    FAmpel(1, 0)
+    basic.pause(5000)
+    AAmpel(1, 1, 0)
+    basic.pause(750)
+    AAmpel(0, 0, 1)
 }
 function FAmpel (Red: number, Green: number) {
     pins.digitalWritePin(DigitalPin.C16, Red)
@@ -14,6 +30,9 @@ function FAmpel (Red: number, Green: number) {
         basic.clearScreen()
     }
 }
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    Wiederholung = true
+})
 function AAmpel (Red: number, Yellow: number, Green: number) {
     pins.digitalWritePin(DigitalPin.P1, Red)
     pins.digitalWritePin(DigitalPin.P0, Yellow)
@@ -21,13 +40,18 @@ function AAmpel (Red: number, Yellow: number, Green: number) {
     if (Red == 1) {
         basic.setLedColor(0xff0000)
     } else if (Yellow == 1) {
-        basic.setLedColor(0xff0000)
+        basic.setLedColor(0xffff00)
     } else if (Green == 1) {
-        basic.setLedColor(0xff0000)
+        basic.setLedColor(0x00ff00)
     } else {
         basic.turnRgbLedOff()
     }
 }
+input.onPinTouchEvent(TouchPin.P3, input.buttonEventDown(), function () {
+    Wiederholung = false
+    Ampelsteuerung()
+})
+let Wiederholung = false
 FAmpel(1, 1)
 AAmpel(1, 1, 1)
 basic.showLeds(`
@@ -37,3 +61,8 @@ basic.showLeds(`
     . # . . .
     . . . # .
     `)
+loops.everyInterval(30000, function () {
+    if (Wiederholung) {
+        Ampelsteuerung()
+    }
+})
