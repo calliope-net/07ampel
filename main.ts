45b92d73b2c83152baa0ab41c6ec39b262e22b22
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-09-13 16:43:31 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,37 @@
+function Ampelsteuerung () {
+	
+}
+function FAmpel (Red: number, Green: number) {
+    pins.digitalWritePin(DigitalPin.C16, Red)
+    pins.digitalWritePin(DigitalPin.C17, Green)
+    if (Red == 1) {
+        basic.showIcon(IconNames.StickFigure)
+    } else if (Green == 1) {
+        basic.showIcon(IconNames.Giraffe)
+    } else {
+        basic.clearScreen()
+    }
+}
+function AAmpel (Red: number, Yellow: number, Green: number) {
+    pins.digitalWritePin(DigitalPin.P1, Red)
+    pins.digitalWritePin(DigitalPin.P0, Yellow)
+    pins.digitalWritePin(DigitalPin.P2, Green)
+    if (Red == 1) {
+        basic.setLedColor(0xff0000)
+    } else if (Yellow == 1) {
+        basic.setLedColor(0xff0000)
+    } else if (Green == 1) {
+        basic.setLedColor(0xff0000)
+    } else {
+        basic.turnRgbLedOff()
+    }
+}
+FAmpel(1, 1)
+AAmpel(1, 1, 1)
+basic.showLeds(`
+    . . . # .
+    . # . . .
+    . . . # .
+    . # . . .
+    . . . # .
+    `)
